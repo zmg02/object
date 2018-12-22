a8659393985484e11b180a9d9c08a83365f14d0a
zmg02 <1225798710@qq.com>
2018-12-22 03:31:56 +0000

    updated database configuration

--- config/database.php
+++ config/database.php
@@ -15,7 +15,7 @@ return [
     |
     */
 
-    'default' => $db_config('connection'),
+    'default' => $db_config['connection'],
 
     /*
     |--------------------------------------------------------------------------
